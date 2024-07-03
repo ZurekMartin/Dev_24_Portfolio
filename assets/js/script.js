ec3dce7783e7ab3007a55f16a66a7226d9cc9864
ZurekMartin <zurek.martin.1@icloud.com>
2024-07-03 19:13:23 +0200

    javascript code initialized

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+const elements = {
+    homeIcon: document.getElementById('home-icon'),
+    themeIcon: document.getElementById('theme-icon'),
+    arrow: document.getElementById('arrow'),
+    mailIcon: document.getElementById('mail-icon'),
+    githubIcon: document.getElementById('github-icon')
+};
+
+function getThemeToSet() {
+    return localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+}
+
+function setTheme(theme, elements) {
+    const mode = `${theme}-mode`;
+    document.body.className = mode;
+    elements.homeIcon.src = `assets/img/home-${mode}.png`;
+    elements.themeIcon.src = `assets/img/${theme === 'dark' ? 'sun' : 'moon'}.png`;
+    localStorage.setItem('theme', theme);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTheme(getThemeToSet(), elements);
+
+    elements.themeIcon.addEventListener('click', () => {
+        const newTheme = document.body.className === 'dark-mode' ? 'light' : 'dark';
+        setTheme(newTheme, elements);
+    });
+});
+
+elements.homeIcon.addEventListener('click', () => window.location.href = 'index.html#hero');
+elements.mailIcon.addEventListener('click', () => window.location.href = `mailto:name@adress.com`);
+
+elements.githubIcon.addEventListener('click', (event) => {
+    event.preventDefault();
+    const win = window.open('https://github.com/ZurekMartin', '_blank');
+    win.focus();
+    win.opener = null;
+});
+
+window.addEventListener('scroll', function () {
+    var arrow = document.getElementById('arrow');
+    var scrollThreshold = window.innerHeight / 16;
+
+    if (window.scrollY > scrollThreshold) {
+        arrow.style.opacity = '0';
+    } else {
+        arrow.style.opacity = '1';
+    }
+});
